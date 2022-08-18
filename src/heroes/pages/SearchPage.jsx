fa293845d3cdc7aef4ec3deb7fd547ca8be75adc
Facundo <floresfacundo68@gmail.com>
2022-08-18 09:53:52 -0300

    Func: agregar busqueda de heroes

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,83 @@
-import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string'
+
+import { useForm } from '../../hooks/useForm';
+import { HeroCard } from '../components/';
+import { getHeroesByName } from '../helpers';
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse( location.search );
+  
+  const { searchText, onInputChange} = useForm({
+    searchText: q
+  });
+  
+  const heroes = getHeroesByName(q);
+
+  const showSearch = ( q.length === 0 );
+  const heroNotFound = ( q.length > 0 && heroes.length === 0 );
+ 
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    const searchQuery = searchText.trim().toLowerCase();
+
+    navigate(`?q=${searchQuery}`)
+  }
+
   return (
-    <h1>SearchPage</h1>
+    <>
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+
+          <form onSubmit={ onSearchSubmit }>
+            <input 
+              type="text" 
+              placeholder="Search a Hero" 
+              className="form-control" 
+              name="searchText" 
+              autoComplete="off"
+              value={ searchText }
+              onChange={ onInputChange }
+            />
+            
+            <button className="btn btn-outline-primary mt-1">
+              Search
+            </button>
+          </form>
+
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+
+
+          <div className="alert alert-primary" style={{display: showSearch ? '' : 'none'}}>
+            Search Hero
+          </div>
+
+          <div className="alert alert-danger" style={{display: heroNotFound ? '' : 'none'}}>
+            No hero with <b>{q}</b>
+          </div>
+
+          {
+            heroes.map( hero => (
+              <HeroCard key={hero.id} {...hero} />
+            ))
+          }
+
+        </div>
+      </div>
+    </>
   )
 }
